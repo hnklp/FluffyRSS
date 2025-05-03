a9858ff5075736854d9a1ad442e12da8943aa514
Hynek Půta <38550797+hnklp@users.noreply.github.com>
2025-05-03 18:10:51 +0200

    Add files via upload

---
+++ FluffyRSS/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FluffyRSS;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ FluffyRSS/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FluffyRSS;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
